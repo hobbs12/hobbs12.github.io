59cb2b8e4377b91f214004b93b01ab4851ef0b0c
Amanda Hobbs <hobbs12@purdue.edu>
2015-02-14 18:32:18 -0500

    First changes
    Add table in html, and JSON table in .js file

--- HW2/assets/js/jshw2.js
+++ HW2/assets/js/jshw2.js
@@ -1,1 +1,16 @@
-console.log('This would be the main JS file.');
+
+$(document).ready(function () {
+	$.getJSON("http://yijisoo.github.io/ie59000/2015spring/hw2/students.json", function (json) {
+    var tr;
+        for (var i = 0; i < json.length; i++) {
+            tr = $('<tr/>');
+            tr.append("<td>" + json[i].Name + "</td>");
+            tr.append("<td>" + json[i].GPA + "</td>");
+            tr.append("<td>" + json[i].GRE_V + "</td>");
+            tr.append("<td>" + json[i].GRE_Q + "</td>");
+            tr.append("<td>" + json[i].Essay+ "</td>");
+            tr.append("<td>" + json[i].Recom+ "</td>");
+            $('#hw2table').append(tr); 
+		});
+		});
+	});
